0af40c3e171a2a8f9c729239b26ae1f88f97493f
Jakaria Blaine <jakariablaine120@gmail.com>
2021-09-10 03:52:01 +0600

    Add some libraries and setup typescript compiler

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,14 @@ module.exports = {
 				test: /\.tsx?$/,
 				use: 'ts-loader',
 				exclude: /node_modules/
+			},
+			{
+			test: /\.s[ac]ss$/i,
+				use: [
+					"style-loader",
+					"css-loader",
+					"sass-loader",
+				],
 			}
 		]
 	},
